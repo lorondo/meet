06ce16060c24594dae8d70ac54120db5798d1b28
lorondo <mralexanderlindsay@gmail.com>
2025-02-03 18:24:41 -0800

    Added service worker and icons

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import * as atatus from 'atatus-spa';
+// import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-atatus.config('14f3d84cff8e4bb79d973da303eb6573').install();
+// atatus.config('14f3d84cff8e4bb79d973da303eb6573').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+serviceWorkerRegistration.register();
