1a7fb85e52b352ec0381172ee0d263bd94db0d2a
lorondo <mralexanderlindsay@gmail.com>
2025-02-05 15:41:59 -0800

    Added offline warning alert

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { extractLocations, getEvents } from './api';
-import { InfoAlert, ErrorAlert } from './components/Alert';
+import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 
 import './App.css';
 
@@ -14,9 +14,15 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
 
   // Fetch events whenever city or number of events changes
   useEffect(() => {
+    if (navigator.onLine) {
+      setWarningAlert(""); // Clear warning if online
+    } else {
+      setWarningAlert("You are currently offline"); // Show warning if offline
+    }
     fetchData();
   }, [currentCity, currentNOE]);
 
@@ -41,6 +47,7 @@ const App = () => {
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
       </div>
       <CitySearch
         allLocations={allLocations}
