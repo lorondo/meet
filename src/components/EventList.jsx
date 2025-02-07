420fd9b0a7f9f0cd13647cb86a5442814d746f23
lorondo <mralexanderlindsay@gmail.com>
2025-02-06 19:52:54 -0800

    Added debug line in EventList

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Event from "./Event";
 
 const EventList = ({ events = [] }) => {
+  console.log("EventList received events:", events); // Debugging line
   return (
     <ul id="event-list" role="list">
       {events.map((event) => (
